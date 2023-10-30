26926a471110e3eaf58e4b2e5745e8ff211ad294
Vadim-Cr <vadim.crudu@gmail.com>
2023-10-30 16:02:42 +0100

    milestone 2, parziale

--- src/Evento.java
+++ src/Evento.java
@@ -47,48 +47,51 @@ public class Evento {
 
 //    METODO PER PRENOTARE
 
-    public void prenotaODisdici(){
+
+    public void prenota(){
         System.out.println("Seats available: " + totalSeats);
         int toReserve = 0;
         // Qui inizia lo Scanner
         Scanner scanner = new Scanner(System.in);
-//todo    *********************************************************************************
 //              QUI CHIEDO SE PRENOTA
         System.out.println("Do you want to reserve some seats? (y/n)");
-       String reservationQuestion= scanner.nextLine();
-      if (reservationQuestion.equalsIgnoreCase("y")){
-               System.out.println("How many seats do you want to reserve?");
-               toReserve += scanner.nextInt();
-               if (toReserve > (totalSeats - reservedSeats)) {
-                   throw new IllegalArgumentException("No more seats available");
-               } else {
-                   System.out.println("Thank you for having used our services, you've reserved: " + toReserve + " seat(s).");
-               }
-          System.out.println(reservedSeats += toReserve);
-//todo    *********************************************************************************
-//              QUI CHIEDO SE DISDICE
-      } else if (reservationQuestion.equalsIgnoreCase("n")){
-               System.out.println("Do you want to cancel your reservation?  (y/n)");
-               String cancelReservation = scanner.nextLine();
-          if (cancelReservation.equalsIgnoreCase("y")){
-              int seatsCanceled = 0;
-              do {
-                  System.out.println("How Many reservations do you want to cancel? ");
-                  seatsCanceled = scanner.nextInt();
-                  if (seatsCanceled <= 0 || seatsCanceled > reservedSeats) {
-                      System.out.println("Invalid input. Please enter a number between 1 and " + reservedSeats);
-                  }
-              } while (seatsCanceled <= 0 || seatsCanceled > reservedSeats);
-
-              System.out.println("You've canceled: " + seatsCanceled);
-              reservedSeats -= seatsCanceled;  // Updating the reservedSeats
-              System.out.println("Total reserved seats now: " + reservedSeats);
-          }
-       }
-
-
-        scanner.close();
+        String reservationQuestion= scanner.nextLine();
+        if (reservationQuestion.equalsIgnoreCase("y")){
+            System.out.println("How many seats do you want to reserve?");
+            toReserve = Integer.parseInt(scanner.nextLine());
+            if (toReserve > (totalSeats - reservedSeats)) {
+                throw new IllegalArgumentException("No more seats available");
+            } else {
+                System.out.println("Thank you for having used our services, you've reserved: " + toReserve + " seat(s).");
+            }
+            System.out.println( "You've reserved: " + (reservedSeats += toReserve) + " Seats");
+        }
     }
+//              QUI CHIEDO SE DISDICE
+          public void disdici () {
+              Scanner scan = new Scanner(System.in);
+              System.out.println("Do you want to cancel some reservations? (y/n)");
+              String cancelReservation = scan.nextLine();
+              if (cancelReservation.equalsIgnoreCase("y")) {
+                  int seatsCanceled = 0;
+                  if (reservedSeats == 0) {
+                      throw new IllegalArgumentException("You cannot cancel as you didn't reserve anything");
+                  } else {
+                      do {
+                          System.out.println("How Many reservations do you want to cancel? ");
+                          seatsCanceled = Integer.parseInt(scan.nextLine());
+                          if (seatsCanceled <= 0 || seatsCanceled > reservedSeats) {
+                              System.out.println("Invalid input. Please enter a number between 1 and " + reservedSeats);
+                          }
+                      } while (seatsCanceled <= 0 || seatsCanceled > reservedSeats);
+
+                      System.out.println("You've canceled: " + seatsCanceled);
+                      reservedSeats -= seatsCanceled;  // Updating the reservedSeats
+                      System.out.println("Total reserved seats now: " + reservedSeats);
+                  }
+              }
+              System.out.println("There are still: " + (totalSeats - reservedSeats) + " seats left");
+          }
 
     @Override
     public String toString() {
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 import java.time.LocalDate;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
     LocalDate dataEvento = LocalDate.of(2023, 11, 15);
-    Evento teatro = new Evento ("Pinocchio",dataEvento, 300);
-    teatro.prenotaODisdici();
+    Evento eventoTeatro = new Evento ("Pinocchio",dataEvento, 300);
+
+//    Chiedo quante volte vuole prenotare
+    Scanner scan=new Scanner(System.in);
+        System.out.println("How many reservations?");
+        int numberOfReservations= scan.nextInt();
+        int reservedSeats=0;
+
+        for (int i = 0; i < (numberOfReservations); i++) {
+            eventoTeatro.prenota();
+            reservedSeats += eventoTeatro.getReservedSeats();
+        }
+        scan.close();
    }
 }
